3a9b91913142d795d0df346353f3934258a3ca7e
amustafayev <afgan.mustafayev@pashabank.az>
2023-04-02 17:56:36 +0400

    Tech: Fix tests and add new tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,11 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('/world (GET)', () => {
+    return request(app.getHttpServer())
+    .get("/world")
+    .expect(200)
+    .expect("Hello World v2")
+  })
 });
